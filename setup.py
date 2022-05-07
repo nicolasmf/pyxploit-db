f4b14b12cb9a7c55dfe48c3a6cd8d9ca4d2b2a4a
Nicolas MF <nikolamf@hotmail.com>
2022-05-07 18:44:00 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyxploitdb",
-    version="1.1",
+    version="1.2",
     description="An exploit-db.com python API using advanced search with all possible filters.",
     long_description=long_description,
     long_description_content_type="text/markdown",
